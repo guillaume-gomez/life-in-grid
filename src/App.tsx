9088804abbaef3feda3441e2f9310a01c47c3a6d
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-02-20 13:46:22 +0100

    remove dead code + create form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
-import React, { useState } from 'react';
-import './App.css';
-import LifeGridCanvas from "./LifeGridCanvas";
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
-const deathAge = 10;
-const periods = [
-  { name: "pre-school", color: "yellow", start: new Date("1995-09-01"), end: new Date("1998-06-01") },
-  { name: "middle-school", color: "green", start: new Date("1998-09-01"), end: new Date("2003-06-01") },
-  { name: "high-school", color: "brown", start: new Date("2003-09-01"), end: new Date("2007-06-01") },
-  { name: "college", color: "purple", start: new Date("2007-09-01"), end: new Date("2010-06-01") },
-  { name: "IT-schools", color: "pink", start: new Date("2010-09-01"), end: new Date("2015-06-01") },
-  { name: "Applidget", color: "#FF09F9", start: new Date("2015-08-01"), end: new Date("2017-11-01") },
-  { name: "Amuse", color: "#F109F9", start: new Date("2017-11-01"), end: new Date("2021-12-15") },
-  { name: "Ecotree", color: "#F10919", start: new Date("2022-01-03"), end: new Date("2022-02-22") },
-]
+import LifeInGridView from "./LifeInGridView";
+import FormView from "./FormView";
+import './App.css';
+
 
 function App() {
-  // february is 1 for month number.
-  const [birthday] = useState<Date>(new Date(1992, 1, 2));
-  const [forecastDeath] = useState<Date>(new Date(1992 + deathAge, 1, 2));
   return (
     <div className="App">
-      <div id="container">
-      <h1 id="title">Your life in grid</h1>
-      <LifeGridCanvas deathDate={forecastDeath} birthdayDate={birthday} periods={periods} />
-      <footer></footer>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LifeInGridView />} />
+          <Route path="create" element={<FormView />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/FormView.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function FormView() {
+  return (
+
+<div className=" relative ">
+    <input type="text" id="simple-email" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Your email"/>
+    </div>
+
+
+  );
+}
+
+export default FormView;

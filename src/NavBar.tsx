70230e1693d0992c76bbb6ce79159eb5027b6cde
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-03-13 03:05:28 +0100

    redirect to /

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+
 import GitHubStarLink from "./GithubStarLink";
 
 function NavBar(): React.ReactElement {
   return (
      <div className="navbar bg-base-300">
         <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">Life in Grid</a>
+          <a href="/" className="btn btn-ghost normal-case text-xl">Life in Grid</a>
         </div>
         <div className="flex-none gap-2">
           <div className="rounded-full">

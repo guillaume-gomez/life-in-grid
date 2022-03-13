43ae0820d2eba0b0293484974bd50173c1622895
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-03-13 03:05:07 +0100

    change date picker

--- src/DatePicker.tsx
+++ src/DatePicker.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import GitHubStarLink from "./GithubStarLink";
 
-function DatePicker(): React.ReactElement {
+import "./DatePicker.css";
 
+interface DatePickerInterface {
+  value: string;
+  onChange: (value : string) => void;
+}
+
+function DatePicker({ value, onChange } : DatePickerInterface): React.ReactElement {
   return (
-    <div>
-      <input type="date" className="text-neutral" />
-    </div>
+    <input
+      type="date"
+      className="rounded-lg bg-base-100 p-4 h-12 border-current"
+      value={value}
+      onChange={(event) => onChange(event.target.value)}
+    />
   );
 }
 

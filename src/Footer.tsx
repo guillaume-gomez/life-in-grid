acbdae2cd755c2474c35db559fadf0259270e385
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-03-14 23:20:12 +0100

    change the font size of 'more info' section

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,8 +3,9 @@ import GitHubStarLink from "./GithubStarLink";
 
 function Footer(): React.ReactElement {
   return (
-    <footer className="p-4 footer bg-base-300 text-base-content footer-center p-6">
+    <footer className="footer bg-base-300 text-base-content footer-center p-6">
       <div>
+        <a href="https://waitbutwhy.com/2014/05/life-weeks.html" className="link text-lg">More information about this project</a>
         <p> 2022 - Made By Guillaume Gomez</p>
         <a href="https://github.com/guillaume-gomez/life-in-grid" className="text-xs">Did you enjoyed ? 😊 Star the project </a>
         <GitHubStarLink />

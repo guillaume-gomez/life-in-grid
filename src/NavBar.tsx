e06e9e4272c725d9bc6a6ba6b5b92f85fc9c69d6
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-03-14 23:20:35 +0100

    increase navbar size

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -4,9 +4,9 @@ import GitHubStarLink from "./GithubStarLink";
 
 function NavBar(): React.ReactElement {
   return (
-     <div className="navbar bg-base-300">
+     <div className="navbar bg-base-300 p-8">
         <div className="flex-1">
-          <a href="/" className="btn btn-ghost normal-case text-xl">Life in Grid</a>
+          <a href="/" className="btn btn-lg btn-ghost normal-case text-xl">Life in Grid</a>
         </div>
         <div className="flex-none gap-2">
           <div className="rounded-full">

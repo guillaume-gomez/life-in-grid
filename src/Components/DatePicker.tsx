a6c7a819afaed6817f6f56cb540cd42899cbf763
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-03-18 01:20:40 +0100

    change datepicker to make it more UX friendly

--- src/Components/DatePicker.tsx
+++ src/Components/DatePicker.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { range } from "lodash";
 import "./DatePicker.css";
 
 interface DatePickerInterface {
@@ -7,14 +7,54 @@ interface DatePickerInterface {
   onChange: (value : string) => void;
 }
 
+
+
+//YEAR-MONTH-DAY
 function DatePicker({ value, onChange } : DatePickerInterface): React.ReactElement {
+  const [maxYear] = useState<number>(new Date().getFullYear() + 100);
+  function getNumberOfDays(month: number): number {
+    if(month === 2) {
+      return 29;
+    }
+    if([1,3,5,7,8, 10, 12].includes(month)) {
+      return 31;
+    }
+    return 30;
+  }
+
+  function getDay(): number {
+    return parseInt(value.split("-")[2], 10);
+  }
+
+  function getMonth(): number {
+    return parseInt(value.split("-")[1], 10);
+  }
+
+  function getYear(): number {
+    return parseInt(value.split("-")[0], 10);
+  }
+
   return (
-    <input
-      type="date"
-      className="rounded-lg bg-base-100 p-4 h-12 border-current"
-      value={value}
-      onChange={(event) => onChange(event.target.value)}
-    />
+    <div className="w-full flex-col md:flex-row flex gap-1">
+      <select className="select max-w-xs" onChange={(event) => onChange(`${event.target.value}-${getMonth()}-${getDay()}`)}>
+        <option disabled selected>Year</option>
+        {range(1950, maxYear).map(year => 
+          <option>{year}</option>
+        )}
+      </select>
+      <select className="select max-w-xs" onChange={(event) => onChange(`${getYear()}-${event.target.value}-${getDay()}`)}>
+        <option disabled selected>Month</option>
+        {range(1, 12).map(month => 
+          <option>{month}</option>
+        )}
+      </select>
+      <select className="select max-w-xs" onChange={(event) => onChange(`${getYear()}-${getMonth()}-${event.target.value}`)}>
+        <option disabled selected>Day</option>
+        {range(1, getNumberOfDays(getDay())).map(day => 
+          <option>{day}</option>
+        )}
+      </select>
+    </div>
   );
 }
 

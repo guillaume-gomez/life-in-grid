101980454f24c622a1c93488c7502b8379b4d2d9
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-03-18 22:54:36 +0100

    move logic into queryStringHelper

--- src/queryStringHelper.ts
+++ src/queryStringHelper.ts
@@ -1,14 +1,38 @@
+import { Period } from "./interfaces";
+
+interface ParsedParamsObjectInterface {
+    birthday: string;
+    "period-length": number;
+    periods: Period[];
+}
+
 const regex = /(.*)\[(.*)\]\[(.*)\]/
 
-export function is2dimArrayKey(keyObj: string) : boolean {
+function is2dimArrayKey(keyObj: string) : boolean {
     return !!keyObj.match(regex);
 }
 
-export function parseArrayKey(keyObj: string) : [string, string, string] {
+function parseArrayKey(keyObj: string) : [string, string, string] {
     const result = keyObj.match(regex);
     if(!result) {
         return ["", "", ""];
     }
     const [_string, key, index, field] = result;
     return [key, index, field];
+}
+
+export function parseParams(params: URLSearchParams, periodLength: number) : ParsedParamsObjectInterface {
+    const periodsArray : Period[] = Array(periodLength).fill(0).map((_val , index) => {
+      return { name: index.toString(), color: "#131963", start: new Date(), end: new Date(), overlap: false }
+    })
+    let queryStringObject : any = { birthday: "", periods: periodsArray, "period-length": periodLength };
+    for(let pair of Array.from(params.entries())) {
+      if(is2dimArrayKey(pair[0])) {
+        const [key, index, field] = parseArrayKey(pair[0]);
+        queryStringObject[key][index][field] = pair[1];
+      } else {
+        queryStringObject[pair[0]] = pair[1];
+      }
+    }
+    return queryStringObject;
 }

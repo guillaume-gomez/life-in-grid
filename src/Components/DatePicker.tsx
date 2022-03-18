f9e9d3f435d7757d4645adecd69ae080e3626de6
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-03-18 14:42:19 +0100

    reverse year + display value

--- src/Components/DatePicker.tsx
+++ src/Components/DatePicker.tsx
@@ -7,9 +7,6 @@ interface DatePickerInterface {
   onChange: (value : string) => void;
 }
 
-
-
-//YEAR-MONTH-DAY
 function DatePicker({ value, onChange } : DatePickerInterface): React.ReactElement {
   const [maxYear] = useState<number>(new Date().getFullYear() + 100);
   function getNumberOfDays(month: number): number {
@@ -36,19 +33,19 @@ function DatePicker({ value, onChange } : DatePickerInterface): React.ReactEleme
 
   return (
     <div className="w-full flex-col md:flex-row flex gap-1">
-      <select className="select max-w-xs" onChange={(event) => onChange(`${event.target.value}-${getMonth()}-${getDay()}`)}>
+      <select className="select max-w-xs" value={getYear()} onChange={(event) => onChange(`${event.target.value}-${getMonth()}-${getDay()}`)}>
         <option disabled selected>Year</option>
-        {range(1950, maxYear).map(year => 
+        {range(1950, maxYear).reverse().map(year => 
           <option>{year}</option>
         )}
       </select>
-      <select className="select max-w-xs" onChange={(event) => onChange(`${getYear()}-${event.target.value}-${getDay()}`)}>
+      <select className="select max-w-xs" value={getMonth()} onChange={(event) => onChange(`${getYear()}-${event.target.value}-${getDay()}`)}>
         <option disabled selected>Month</option>
         {range(1, 12).map(month => 
           <option>{month}</option>
         )}
       </select>
-      <select className="select max-w-xs" onChange={(event) => onChange(`${getYear()}-${getMonth()}-${event.target.value}`)}>
+      <select className="select max-w-xs" value={getDay()} onChange={(event) => onChange(`${getYear()}-${getMonth()}-${event.target.value}`)}>
         <option disabled selected>Day</option>
         {range(1, getNumberOfDays(getDay())).map(day => 
           <option>{day}</option>

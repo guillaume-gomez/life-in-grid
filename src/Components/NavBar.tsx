2b7ad72396c6ce2818d0a8f779a42507ef672642
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-07-02 19:54:32 +0200

    fix redirection to portfolio

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -6,7 +6,7 @@ function NavBar(): React.ReactElement {
   return (
      <div className="navbar bg-base-300 p-8">
         <div className="flex-1">
-          <a href="/" className="btn btn-lg btn-ghost normal-case text-xl">Life in Grid</a>
+          <a href="/life-in-grid" className="btn btn-lg btn-ghost normal-case text-xl">Life in Grid</a>
         </div>
         <div className="flex-none gap-2">
           <div className="rounded-full">
